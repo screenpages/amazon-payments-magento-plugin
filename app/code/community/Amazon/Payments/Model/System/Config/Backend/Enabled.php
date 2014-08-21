987c1ff01305c170c0401f11b83e5655a069dbc9
Jonah Ellison <jonahellison@gmail.com>
2014-08-21 14:48:30 -0700

    fix #42 - validate seller id as alphanumeric

--- app/code/community/Amazon/Payments/Model/System/Config/Backend/Enabled.php
+++ app/code/community/Amazon/Payments/Model/System/Config/Backend/Enabled.php
@@ -11,13 +11,54 @@
 class Amazon_Payments_Model_System_Config_Backend_Enabled extends Mage_Core_Model_Config_Data
 {
     /**
-     * Perform API call to Amazon to validate Client ID/Secret
-     *
+     * Validate data
      */
     public function save()
     {
-        //return Mage::getModel('amazon_login/system_config_backend_enabled')->save();
+
+        $data = $this->getFieldsetData();
+        $isEnabled = $this->getValue();
+
+        if ($isEnabled) {
+            if ($data['seller_id'] && !ctype_alnum($data['seller_id'])) {\
+                Mage::getSingleton('core/session')->addError('Error: Please verify your Seller ID (alphanumeric characters only).');
+            }
+        }
         return parent::save();
     }
+
+
+    /**
+     * Perform API call to Amazon to validate keys
+     *
+     */
+    /*
+    public function _afterSaveCommit()
+    {
+
+        $data = $this->getFieldsetData();
+        $isEnabled = $this->getValue();
+        $idTest = 'S23-1234567-1234567';
+
+        if ($isEnabled) {
+            $_api = Mage::getSingleton('amazon_payments/api');
+
+            try {
+                $result = $_api->getOrderReferenceDetails($idTest);
+            }
+            catch (Exception $e) {
+                if (strpos($e->getMessage(), $idTest) === FALSE) {
+                    $matches = array();
+                    preg_match("/'(.*)'/", $e->getMessage(), $matches);
+                    if ($matches[0]) {
+                        Mage::getSingleton('core/session')->addError('Error: ' . $matches[0]);
+                    }
+
+                }
+            }
+        }
+
+        return parent::_afterSaveCommit();
+    }
+    */
 }
-?>
