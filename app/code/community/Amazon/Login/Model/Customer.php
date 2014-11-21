1b4077296f92c3c286474f35899cd07112bbafe9
Matt Neilson <mattneil@amazon.com>
2014-11-20 18:32:47 -0800

    Fixing spacing issue.

--- app/code/community/Amazon/Login/Model/Customer.php
+++ app/code/community/Amazon/Login/Model/Customer.php
@@ -75,7 +75,7 @@ class Amazon_Login_Model_Customer extends Mage_Customer_Model_Customer
      */
     public function getAmazonName($name)
     {
-	// if the user only has a first name, handle accordingly
+        // if the user only has a first name, handle accordingly
         $trimmedName = trim($name);
         if(strpos($trimmedName,' ')===false) {
             return array($trimmedName,'.');
