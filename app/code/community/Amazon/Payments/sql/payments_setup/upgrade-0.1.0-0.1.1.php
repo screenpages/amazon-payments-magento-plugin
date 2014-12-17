dfe1fe1725b771117f8de4db866d90480e3b5859
jonahellison <jonahellison@gmail.com>
2014-12-17 12:09:32 -0800

    fix token table creation

--- app/code/community/Amazon/Payments/sql/payments_setup/upgrade-0.1.0-0.1.1.php
+++ app/code/community/Amazon/Payments/sql/payments_setup/upgrade-0.1.0-0.1.1.php
@@ -14,7 +14,7 @@ $installer = $this;
 $installer->startSetup();
 
 $amazon_table = $installer->getConnection()
-    ->newTable($installer->getTable('amazon_payments_token'))
+    ->newTable($installer->getTable('amazon_payments/token'))
     ->addColumn('token_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'identity' => true,
         'unsigned' => true,
