2e7952dcd3ee1a207970aef411c0829a489e5666
jonahellison <jonahellison@gmail.com>
2015-03-18 09:31:46 -0700

    #145 use amazon_login settings path for client id/secret

--- app/code/community/Amazon/Payments/Model/Config.php
+++ app/code/community/Amazon/Payments/Model/Config.php
@@ -15,8 +15,8 @@ class Amazon_Payments_Model_Config
      */
 
     const CONFIG_XML_PATH_ENABLED        = 'payment/amazon_payments/enabled';
-    const CONFIG_XML_PATH_CLIENT_ID      = 'payment/amazon_payments/client_id';
-    const CONFIG_XML_PATH_CLIENT_SECRET  = 'payment/amazon_payments/client_secret';
+    const CONFIG_XML_PATH_CLIENT_ID      = 'amazon_login/settings/client_id';
+    const CONFIG_XML_PATH_CLIENT_SECRET  = 'amazon_login/settings/client_secret';
     const CONFIG_XML_PATH_SELLER_ID      = 'payment/amazon_payments/seller_id';
     const CONFIG_XML_PATH_ACCESS_KEY     = 'payment/amazon_payments/access_key';
     const CONFIG_XML_PATH_ACCESS_SECRET  = 'payment/amazon_payments/access_secret';
