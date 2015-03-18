7e74e6f614e927318f1753191f8a71a738530641
jonahellison <jonahellison@gmail.com>
2015-03-18 15:44:23 -0700

    fix #132 - add 'None' (new) payment action

--- app/code/community/Amazon/Payments/Model/System/Config/Source/Paymentaction.php
+++ app/code/community/Amazon/Payments/Model/System/Config/Source/Paymentaction.php
@@ -15,6 +15,7 @@ class Amazon_Payments_Model_System_Config_Source_Paymentaction
         return array(
             array('value'=>'authorize_capture', 'label'=>Mage::helper('adminhtml')->__('Authorize and Capture')),
             array('value'=>'authorize', 'label'=>Mage::helper('adminhtml')->__('Authorize Only')),
+            array('value'=>'new', 'label'=>Mage::helper('adminhtml')->__('None')),
         );
     }
 }
