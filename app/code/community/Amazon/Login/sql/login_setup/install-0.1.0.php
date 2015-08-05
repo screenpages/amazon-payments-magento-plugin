69071e66b89d8477b19277252450efc5ecd9d01c
Rahul Mode <mode@amazon.com>
2015-08-05 11:25:20 -0700

    Added

--- app/code/community/Amazon/Login/sql/login_setup/install-0.1.0.php
+++ app/code/community/Amazon/Login/sql/login_setup/install-0.1.0.php
@@ -12,35 +12,39 @@ $installer = $this;
 
 $installer->startSetup();
 
-$amazon_table = $installer->getConnection()
-    ->newTable($installer->getTable('amazon_login/login'))
-    ->addColumn('login_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-        'identity' => true,
-        'unsigned' => true,
-        'nullable' => false,
-        'primary'  => true
-    ), 'Login ID')
-    ->addColumn('customer_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array (
-        'nullable' => false,
-        'unsigned' => true
-    ), 'Customer Entity ID')
-    ->addColumn('amazon_uid', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array (
-        'nullable' => true,
-        'unsigned' => true
-    ), 'Amazon User ID')
-    ->addIndex($installer->getIdxName('amazon_login/login', array('customer_id')), array('customer_id'))
-    ->addIndex($installer->getIdxName('amazon_login/login', array('amazon_uid')), array('amazon_uid'));
+$amazon_login_table = $installer->getTable('amazon_login/login');
 
-$installer->getConnection()->createTable($amazon_table);
+if ($installer->getConnection()->isTableExists($amazon_login_table) != true) {
 
-$installer->getConnection()->addConstraint(
-    'fk_amazon_login_customer_entity_id',
-    $installer->getTable('amazon_login/login'),
-    'customer_id',
-    $installer->getTable('customer/entity'),
-    'entity_id',
-    'cascade',
-    'restrict'
-);
-
+    $amazon_table = $installer->getConnection()
+        ->newTable($installer->getTable('amazon_login/login'))
+        ->addColumn('login_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'identity' => true,
+            'unsigned' => true,
+            'nullable' => false,
+            'primary'  => true
+        ), 'Login ID')
+        ->addColumn('customer_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array (
+            'nullable' => false,
+            'unsigned' => true
+        ), 'Customer Entity ID')
+        ->addColumn('amazon_uid', Varien_Db_Ddl_Table::TYPE_VARCHAR, 255, array (
+            'nullable' => true,
+            'unsigned' => true
+        ), 'Amazon User ID')
+        ->addIndex($installer->getIdxName('amazon_login/login', array('customer_id')), array('customer_id'))
+        ->addIndex($installer->getIdxName('amazon_login/login', array('amazon_uid')), array('amazon_uid'));
+    
+    $installer->getConnection()->createTable($amazon_table);
+    
+    $installer->getConnection()->addConstraint(
+        'fk_amazon_login_customer_entity_id',
+        $installer->getTable('amazon_login/login'),
+        'customer_id',
+        $installer->getTable('customer/entity'),
+        'entity_id',
+        'cascade',
+        'restrict'
+    );
+}
 $installer->endSetup();
